9a4e8495779208aa060b61136b13c8c24f97368f
Sebastian David Lees <sebastian@incerto.net>
2018-09-21 16:34:28 +0100

    Test skeletons added.

--- scraper/tests.py
+++ scraper/tests.py
@@ -1,2 +1,35 @@
 # -*- coding: utf-8 -*-
 # The soybean condition scraper tests.
+
+import pytest
+
+from scraper.scraper import get_zip_files_links, extract_zip, get_soybean_data,\
+    clean_soybean_data, create_output_file, write_to_output_file, clean_week_ending
+
+
+def test_get_zip_files_links():
+    pass
+
+
+def test_extract_zip():
+    pass
+
+
+def test_get_soybean_data():
+    pass
+
+
+def test_clean_soybean_data():
+    pass
+
+
+def test_create_output_file():
+    pass
+
+
+def test_write_to_output_file():
+    pass
+
+
+def test_clean_week_ending():
+    pass
